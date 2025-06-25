2524a68f6ada8f3f80aca57f95efc987c61ef896
moriagr <moriagro@gmail.com>
2025-06-25 16:05:57 +0300

    fix client-server connection

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 console.log(process.env)
 export default axios.create({
-    baseURL: process.env.REACT_APP_API_URL + "api",
+    baseURL: process.env.REACT_APP_API_URL + "/api",
     headers: {
         'Content-Type': 'application/json'
     }
